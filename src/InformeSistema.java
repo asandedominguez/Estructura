970d79de1b5b1ef5cc0e5a186650749a2dc0d48a
Adrián <asandedominguez@danielcastelao.org>
2026-09-17 12:50:01 +0200

    Primer ejercicio completado - segundo parte 1 completado

--- src/InformeSistema.java
+++ src/InformeSistema.java
@@ -5,13 +5,31 @@ public class InformeSistema {
         return procesadores;
     }
 
-    public static int mReservada(){
+    public static long mTotal() {
+        long total = Runtime.getRuntime().totalMemory();
+        return total;
+    }
 
+    public static long mLibre(){
+        long libre = Runtime.getRuntime().freeMemory();
+        return libre;
+    }
+
+    public static long max() {
+        long maximo = Runtime.getRuntime().maxMemory();
+        return maximo;
+    }
+
+    public static long mUso() {
+        long uso = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        return uso;
     }
 
     public static void main (String[] args) {
         System.out.println("PROCESADORES " +
                 "\n ---------------- " +
                 "\n Disponibles JVM: " + InformeSistema.nProcesadores());
+
+        System.out.println(InformeSistema.mTotal());
     }
 }
